36cf770be8d2a0425cbb32c7835e281d47beb4d7
isarbrj <isabella7ma@hotmail.com>
2024-07-17 19:41:37 -0300

    Primeiro commit

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -8,10 +8,37 @@ defmodule DesafioCli do
   comando como lista de strings e executa a CLI.
   """
   def main(_args) do
-    IO.puts(phrase())
+    IO.puts(
+      "Escreva os nomes dos reis, um por linha, ao final deixe uma linha em branco e dê enter:"
+    )
+
+    le_lista([])
   end
 
-  def phrase() do
-    "Hello, world!"
+  @romanos [{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"}, {100, "C"}, {500, "D"}, {1000, "M"}]
+
+  def valor_em_romano(valor, romano) do
+    if valor == 0 do
+      romano
+    else
+      {arabico, algarismo} = Enum.find(Enum.reverse(@romanos), fn {arabico, _} -> valor >= arabico end)
+      valor_em_romano(valor-arabico,"#{romano}#{algarismo}")
+    end
+
+  end
+
+  def le_lista(lista) do
+    nome = IO.gets("")
+
+    if nome == "\n" do
+        Enum.reduce(lista, %{}, fn pessoa, contagem ->
+          conte = Map.update(contagem, pessoa, 1, fn val -> val + 1 end)
+          IO.puts("#{pessoa} #{valor_em_romano(Map.get(conte, pessoa),"")}")
+          conte
+        end)
+    else
+      nova_lista = lista ++ [String.trim(nome)]
+      le_lista(nova_lista)
+    end
   end
 end
--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -2,7 +2,11 @@ defmodule DesafioCliTest do
   use ExUnit.Case
   doctest DesafioCli
 
-  test "greets the world" do
-    assert DesafioCli.phrase() == "Hello, world!"
+  test "1501 em romano" do
+    assert DesafioCli.valor_em_romano(1501, "") == "MDI"
+  end
+
+  test "1523 em romano" do
+    assert DesafioCli.valor_em_romano(1523, "") == "MDXXIII"
   end
 end
